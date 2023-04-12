9892a71325675726159b5244a721bc0e13daa612
wangtao <wangtao@dihuge.com>
2023-04-12 16:38:48 +0800

    删除原来的

--- cc/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#include "str/str_utils.h"
-
-int main(int argc, char **argv)
-{
-    std::cout << strip("abcd ") << std::endl;;
-    return 0;
-}
--- cc/str/str_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef STR_UTILS_H
-#define STR_UTILS_H
-
-#include <string>
-
-std::string strip(const std::string &str);
-
-#endif
