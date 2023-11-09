fff22f550932eba31e275398131d3afa0cdb838b
wangtao <wangtao@dihuge.com>
2023-11-09 10:38:24 +0800

    添加opencv配置，打印对应的配置

---
+++ cmake/ConfigOpenCV.cmake
@@ -0,0 +1,6 @@
+set(OpenCV_VERSION "4.5.0")
+set(OpenCV_DIR "D:/Software/dev/opencv-4.5.0")
+set(OpenCV_INCLUDE_DIRS "D:/Software/dev/opencv-4.5.0/include")
+list(APPEND OpenCV_LIBS ${OpenCV_DIR}/lib/opencv_world450d.lib)
+# find_package(OpenCV REQUIRED HINTS ${OpenCV_DIR} PATH_SUFFIXES lib) 
+
