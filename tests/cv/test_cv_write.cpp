7a66fe063c6314dba9fb75f653d45edd652a9638
wangtao <wangtao@dihuge.com>
2023-11-10 16:59:13 +0800

    调整测试，cmake编译后copy相应的dll到目标目录

--- tests/cv/test_cv_write.cpp
+++ tests/cv/test_cv_write.cpp
@@ -7,20 +7,13 @@
 
 // clang-format off
 
-std::vector<std::vector<std::string>> testdata = {
-    std::vector<std::string>{"test.png"},
-    std::vector<std::string>{"test.jpg"},
-    std::vector<std::string>{"test.tiff"},
-    std::vector<std::string>{"test.bmp"},
-};
-
-FREE_KICK_TEST_SUITE_P(OpenCVImwriteTest, ValueList<std::string&, int>
+FREE_KICK_TEST_SUITE_P(OpenCVImwriteTest, ValueList<std::string&, bool>
 {
     // ext
-    { "test.png",  5 },
-    { "test.jpg",  5 },
-    { "test.tiff", 2 },
-    { "test.bmp",  3 },
+    { "test.png",  false },
+    { "test.jpg",  false },
+    { "test.tiff", true },
+    { "test.bmp",  false },
 });
 
 // clang-format on
@@ -28,15 +21,20 @@ FREE_KICK_TEST_SUITE_P(OpenCVImwriteTest, ValueList<std::string&, int>
 TEST_P(OpenCVImwriteTest, ext)
 {
     std::string ext  = GetParamValue<0>();
-    int         size = GetParamValue<1>();
+    bool        cond = GetParamValue<1>();
 
     std::string img_path = "test." + ext;
 
-    cv::Mat src(size, size, CV_32FC1, cv::Scalar(22.5));
+    cv::Mat src(20, 20, CV_32FC1, cv::Scalar(22.5));
     cv::imwrite(img_path, src);
     cv::Mat dst = cv::imread(img_path, cv::IMREAD_UNCHANGED);
-    EXPECT_EQ(src.cols, dst.cols);
-    EXPECT_EQ(src.rows, dst.rows);
-    EXPECT_EQ(src.type(), dst.type());
-    EXPECT_EQ(src.at<float>(0, 0), dst.at<float>(0, 0));
+    EXPECT_EQ(src.cols == dst.cols, true);
+    EXPECT_EQ(src.rows == dst.rows, true);
+    EXPECT_EQ(src.type() == dst.type(), cond);
+    EXPECT_TRUE(src.depth() == CV_32F);
+    EXPECT_EQ(dst.depth() == CV_32F, cond);
+    if (dst.depth() == CV_32F)
+    {
+        EXPECT_FLOAT_EQ(src.at<float>(0, 0), dst.at<float>(0, 0));
+    }
 }
