5c8e4e27336aaccce2479f29eddc84f6e66b66ed
wangtao <wangtao@dihuge.com>
2023-11-10 16:58:16 +0800

    调整opencv配置

--- cmake/ConfigOpenCV.cmake
+++ cmake/ConfigOpenCV.cmake
@@ -1,6 +1,4 @@
-set(OpenCV_VERSION "4.5.0")
-set(OpenCV_DIR "D:/Software/dev/opencv-4.5.0")
-set(OpenCV_INCLUDE_DIRS "D:/Software/dev/opencv-4.5.0/include")
-list(APPEND OpenCV_LIBS ${OpenCV_DIR}/lib/opencv_world450d.lib)
-# find_package(OpenCV REQUIRED HINTS ${OpenCV_DIR} PATH_SUFFIXES lib) 
-
+set(OpenCV_DIR "D:/Software/dev/opencv-4.5.5/build/x64/vc14/lib")
+set(OpenCV_LIBRARY_DIR ${OpenCV_DIR})
+set(OpenCV_BIN_DIR "D:/Software/dev/opencv-4.5.5/build/x64/vc14/bin")
+find_package(OpenCV REQUIRED) 
