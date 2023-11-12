e534a6178027b3baaf503f4cf48f60e83f47b9a2
wangtao <595517713@qq.com>
2023-11-12 14:09:15 +0800

    修改为参数化测试

--- tests/utils/test_str_utils.cpp
+++ tests/utils/test_str_utils.cpp
@@ -1,46 +1,54 @@
-#include <gtest/gtest.h>
+#include "../values_test.h"
 #include "str_utils.h"
 
+#include <gtest/gtest.h>
+
 // using free_kick::strip;
-using namespace free_kick;
-        
-TEST(StrTest, strip_test_space)
-{
-    ASSERT_EQ(strip(" abcd"), "abcd");
-    ASSERT_EQ(strip(" abcd "), "abcd");
-    ASSERT_EQ(strip("  abcd "), "abcd");
-    ASSERT_EQ(strip("  ab cd "), "ab cd");
-    ASSERT_EQ(strip("  ab  cd "), "ab  cd");
-    ASSERT_EQ(strip("      a b  c d     "), "a b  c d");
+using free_kick::utils::strip;
 
-    ASSERT_NE(strip(" abcd"), " abcd");
-    ASSERT_NE(strip("abcd   "), "abcd   ");
-    ASSERT_NE(strip("   abcd   "), "   abcd   ");
-    ASSERT_NE(strip(" abcd "), " abcd ");
-    ASSERT_NE(strip(" ab cd "), "ab  cd");
+// clang-format off
+
+FREE_KICK_TEST_SUITE_P(StripEQTest, ValueList<std::string &, std::string>
+{
+//                   input,     result
+    {              " abcd",     "abcd"},
+    {             " abcd ",     "abcd"},
+    {            "  abcd ",     "abcd"},
+    {           "  ab cd ",    "ab cd"},
+    {          "  ab  cd ",   "ab  cd"},
+    {"      a b  c d     ", "a b  c d"},
+
+    {             "\tabcd",     "abcd"},
+    {     "\t\tabcd\t\t\t",     "abcd"},
+
+    {             "\nabcd",     "abcd"},
+    {      "\n\nab cd\n\n",    "ab cd"},
+});
+
+FREE_KICK_TEST_SUITE_P(StripNEQTest, ValueList<std::string &, std::string>
+{
+//           input,       result
+    {      " abcd",      " abcd"},
+    {    "abcd   ",    "abcd   "},
+    { "   abcd   ", "   abcd   "},
+    {     " abcd ",     " abcd "},
+    {    " ab cd ",     "ab cd "},
+
+    {     "\tabcd",     "\tabcd"},
+    {   "\tabcd\t",   "\tabcd\t"},
+
+    {    "\nab cd",    "\nab cd"},
+    {  "\nab cd\n",  "\nab cd\n"},
+});
+
+// clang-format on
+
+TEST_P(StripEQTest, strip_test_eq)
+{
+    EXPECT_EQ(strip(GetParamValue<0>()), GetParamValue<1>());
 }
 
-TEST(StrTest, strip_test_tab)
+TEST_P(StripNEQTest, strip_test_neq)
 {
-    ASSERT_EQ(strip("\tabcd"), "abcd");
-    ASSERT_EQ(strip("\tabcd\t"), "abcd");
-    ASSERT_EQ(strip("\ta\tbc\td\t"), "a\tbc\td");
-    ASSERT_EQ(strip("\t\tabcd\t\t\t"), "abcd");
-
-    ASSERT_NE(strip("\tabcd"), "\tabcd");
-    ASSERT_NE(strip("\tabcd\t"), "\tabcd\t");
-    ASSERT_NE(strip("\t\tabcd\t\t\t"), "\t\tabcd\t\t\t");
-}
-
-TEST(StrTest, strip_test_newline)
-{
-    ASSERT_EQ(strip("\nabcd"), "abcd");
-    ASSERT_EQ(strip("abcd\n"), "abcd");
-    ASSERT_EQ(strip("ab\ncd"), "ab\ncd");
-    ASSERT_EQ(strip("\n\nabcd\n\n"), "abcd");
-    ASSERT_EQ(strip("\n\nab cd\n\n"), "ab cd");
-
-    ASSERT_NE(strip("\nab cd"), "\nab cd");
-    ASSERT_NE(strip("ab cd\n"), "ab cd\n");
-    ASSERT_NE(strip("\nab cd\n"), "\nab cd\n");
+    EXPECT_NE(strip(GetParamValue<0>()), GetParamValue<1>());
 }
