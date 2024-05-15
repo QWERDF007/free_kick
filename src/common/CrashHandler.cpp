daae28a523b49d885f018a6d9f0c80c688846cf1
wangtao <wangtao@dihuge.com>
2024-05-15 09:50:29 +0800

    根据预编译条件安装CCrashHandler

--- src/common/CrashHandler.cpp
+++ src/common/CrashHandler.cpp
@@ -3,7 +3,7 @@
 #if defined(_WIN32)
 #    include "WindowsCCrashHandler.h"
 #else
-
+#    include "LinuxCCrashHandler.h"
 #endif
 
 namespace free_kick::common {
@@ -14,7 +14,8 @@ void CrashHandler::setup()
     WindowsCCrashHandler ccrash_handler;
     ccrash_handler.setup();
 #else
-
+    LinuxCCrashHandler ccrash_handler;
+    ccrash_handler.setup();
 #endif
 }
 
