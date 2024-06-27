ba754e76f26e74c2eb07556e91b7665f4601d32b
wangtao <wangtao@dihuge.com>
2024-06-27 15:34:42 +0800

    修改sqlpp11示例

--- samples/sqlpp11_samples/sqlite3_insert/main.cpp
+++ samples/sqlpp11_samples/sqlite3_insert/main.cpp
@@ -6,25 +6,66 @@
 
 #include <iostream>
 
+std::vector<std::string> getAllTables(sqlpp::sqlite3::connection &conn)
+{
+    std::vector<std::string> tables_names;
+
+    if (!conn.is_connected())
+    {
+        std::cerr << "Not connected to database" << std::endl;
+        return tables_names;
+    }
+    auto db = conn.native_handle();
+
+    sqlite3_stmt *stmt = nullptr;
+    int           rc = sqlite3_prepare_v2(db, "SELECT name FROM sqlite_master WHERE type='table';", -1, &stmt, nullptr);
+    if (rc != SQLITE_OK)
+    {
+        std::cerr << "Could not prepare statement: " << sqlite3_errmsg(db) << std::endl;
+        return tables_names;
+    }
+    while (sqlite3_step(stmt) == SQLITE_ROW)
+    {
+        // 取出第0列字段的值
+        std::string name = (char *)sqlite3_column_text(static_cast<sqlite3_stmt *>(stmt), 0);
+        tables_names.emplace_back(name);
+    }
+    sqlite3_finalize(stmt);
+    return tables_names;
+}
+
 int main(int argc, char *argv[])
 {
     try
     {
         sqlpp::sqlite3::connection_config config;
-        config.path_to_database = ":memory:"; // db on memory
-        // config.path_to_database = "test.db"; // db on disk
-        config.flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
-        config.debug = false;
+        // config.path_to_database = ":memory:"; // db on memory
+        config.path_to_database = "test.db"; // db on disk
+        config.flags            = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
+        config.debug            = false;
         sqlpp::sqlite3::connection db(config);
         if (!db.is_connected())
         {
             std::cerr << "Could not connect to database" << std::endl;
             return 1;
         }
-        db.execute(R"(CREATE TABLE "table_to_insert" (
-            "id"	INTEGER NOT NULL,
-            PRIMARY KEY("id" AUTOINCREMENT)
-        );)");
+        auto tables_names = getAllTables(db);
+        bool table_exists = false;
+        for (const auto table : tables_names)
+        {
+            if (table == "table_to_insert")
+            {
+                table_exists = true;
+                break;
+            }
+        }
+        if (!table_exists)
+        {
+            db.execute(R"(CREATE TABLE "table_to_insert" (
+                "id"	INTEGER NOT NULL,
+                PRIMARY KEY("id" AUTOINCREMENT)
+            );)");
+        }
         std::cout << "Table created" << std::endl;
         // table::TableToInsert table;
         table::TableToInsert table;
