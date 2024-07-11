a2efe26c869bc4cf1b96ea21ce8c0ce6d695a523
wangtao <wangtao@dihuge.com>
2024-07-11 17:28:25 +0800

    修改cmake，添加free_kick前缀

--- tests/qt/test_qstr_utils.cpp
+++ tests/qt/test_qstr_utils.cpp
@@ -13,6 +13,9 @@ FREE_KICK_TEST_SUITE_P(Vec2QStrTest, ValueList<const std::vector<double>&, const
     {{1.,2.,3.}, "1, 2, 3, "},
     {{-1.,-2.,-3.}, "-1, -2, -3, "},
     {{0.,0.,0.}, "0, 0, 0, "},
+    {{0.5,0.6,0.7}, "0.5, 0.6, 0.7, "},
+    {{0.1,0.2,0.3,0.4,0.5,0.6,0.7}, "0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, "},
+    {{0.11,0.22,0.33,0.44,0.55,0.66,0.77}, "0.11, 0.22, 0.33, 0.44, 0.55, 0.66, 0.77, "},
 });
 
 // clang-format on
@@ -20,4 +23,9 @@ FREE_KICK_TEST_SUITE_P(Vec2QStrTest, ValueList<const std::vector<double>&, const
 TEST_P(Vec2QStrTest, ToQStr)
 {
     EXPECT_EQ(toQString(GetParamValue<0>()).toStdString(), GetParamValue<1>());
+}
+
+TEST_P(Vec2QStrTest, ToQStrv2)
+{
+    EXPECT_EQ(toQStringv2(GetParamValue<0>()).toStdString(), GetParamValue<1>());
 }
